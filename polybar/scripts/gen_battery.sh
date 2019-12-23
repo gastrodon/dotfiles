7054a0600d38737e5925001a503c05797f4f3ad7
Noah Harris <jnharris@boomtownroi.com>
2019-12-23 14:52:59 -0500

    I messed up scripts folder

--- polybar/scripts/gen_battery.sh
+++ polybar/scripts/gen_battery.sh
@@ -1,27 +1,39 @@
-charged=$(expr $(cat /sys/class/power_supply/BAT0/capacity) / 10)
-discharged=$(expr 10 - $charged)
-
 charged_bar=""
 charged_fill=""
 
+if [ $(cat /sys/class/power_supply/BAT0/status) = "Unknown" ]; then
+	active_battery="BAT1"
+else
+	active_battery="BAT0"
+fi
+
+charged=$(expr $(cat /sys/class/power_supply/$active_battery/capacity) / 10)
+discharged=$(expr 10 - $charged)
+
+if [ $(cat /sys/class/power_supply/$active_battery/status) = "Unknown" ]; then
+	charged_bar="?"
+else
+	charged_bar="|"
+fi
+
 while [ "$charged" -ne "0" ]; do
-	charged_bars="$charged_bars|"
+	charged_bars="$charged_bars$charged_bar"
 	charged=$(expr $charged - 1)
 done
 
 while [ "$discharged" -ne "0" ]; do
-	charged_bars="$(echo $charged_bars)."
+	charged_bars="$charged_bars."
 	discharged=$(expr $discharged - 1)
 done
 
-color="F#FFFFFF"
+color="F#B48EAD"
 
-if [ $(cat /sys/class/power_supply/BAT0/capacity) -lt 10 ]; then
-	color="F#FF0000"
+if [ $(cat /sys/class/power_supply/$active_battery/capacity) -lt 10 ]; then
+	color="F#BF616A"
 fi
 
-if [ $(cat /sys/class/power_supply/BAT0/status) = "Charging" ]; then
-	color="F#00FF00"
+if [ $(cat /sys/class/power_supply/$active_battery/status) = "Charging" ]; then
+	color="F#A3BE8C"
 fi
 
 echo "%{$color}$charged_bars%{F-}"
--- polybar/scripts/gen_wifi.sh
+++ polybar/scripts/gen_wifi.sh
@@ -3,9 +3,9 @@ ssid=$(nmcli -t -f active,ssid dev wifi | egrep ^yes | tr ":" "\n" | sed '2q;d')
 connected=$(nmcli -t -f active,ssid dev wifi | egrep ^yes | tr ":" "\n\n" | sed '1q;d')
 
 if [ -f /sys/class/net/tun0/dev_id ]; then
-	color="F#00FF00"
+	color="F#A3BE8C"
 else
-	color="F#FFFFFF"
+	color="F#B48EAD"
 fi
 
 if [ $(echo $ssid | wc -c) -eq 1 ]; then
