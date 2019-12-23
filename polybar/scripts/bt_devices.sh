2fa98f8f9ee374744a116c3f1f719f018df5b7ab
Noah Harris <jnharris@boomtownroi.com>
2019-12-23 14:57:46 -0500

    Use correct color in disconnected bluetooth
    Disconnected but powered on bluetooth indicator was using the wrong
    color because I can't read, B4BEAD -> B48EAD

--- polybar/scripts/bt_devices.sh
+++ polybar/scripts/bt_devices.sh
@@ -1,7 +1,7 @@
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
 	color="#666666"
 else
-	color="#B4BEAD"
+	color="#B48EAD"
 fi
 
 connection=$(bluetoothctl info | grep Alias | cut -d':' -f2)
