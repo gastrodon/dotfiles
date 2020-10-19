4f0f81afa4243ebf540bc7dc21a4d4f3f13f2c65
Noah Harris <dakoolstwunn@gmail.com>
2020-10-19 18:20:27 -0400

    Add color to ip script

--- config/polybar/scripts/gen_ip.sh
+++ config/polybar/scripts/gen_ip.sh
@@ -1,1 +1,4 @@
-ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+ip="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+color="#B48EAD"
+
+echo "%{F$color}$ip%{F-}"
