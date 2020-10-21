73eba335481add48c63dcb4ad317ac0dd49ca32a
Noah Harris <dakoolstwunn@gmail.com>
2020-10-21 10:59:17 -0400

    Add vpn color to gen_ip

--- config/polybar/scripts/gen_ip.sh
+++ config/polybar/scripts/gen_ip.sh
@@ -1,4 +1,9 @@
 ip="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
-color="#B48EAD"
+
+if [ -f /sys/class/net/tun0/dev_id ]; then
+	color="#A3BE8C"
+else
+	color="#B48EAD"
+fi
 
 echo "%{F$color}$ip%{F-}"
