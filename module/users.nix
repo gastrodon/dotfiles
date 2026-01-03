fa59b1fdcdcf4f4b1e9cda22d49a3f01e9abecb4
eva <mail@gastrodon.io>
2026-01-03 01:53:47 -0500

    tidy configs
    Remove plenty of default comments, fill out some previously defaulted
    options, add some more packages to user

--- module/users.nix
+++ module/users.nix
@@ -3,6 +3,12 @@
   users.users.eva = {
     isNormalUser = true;
     description = "eva";
+    packages = with pkgs; [
+      bottom
+      tldr
+      ripgrep
+    ];
+
     extraGroups = [
       "wheel"          # Enable sudo
       "networkmanager" # Manage network connections
@@ -12,6 +18,5 @@
 
     # Set a real password with `sudo passwd eva`
     initialPassword = "foobar2000";
-    hashedPassword = null;
   };
 }
