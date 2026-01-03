523c8b454b42f73f1ca1af7e5da1190d2100359b
eva <mail@gastrodon.io>
2026-01-03 11:36:56 -0500

    tidy: trim comments + nixfmt

--- configuration.nix
+++ configuration.nix
@@ -1,19 +1,22 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
 in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./module/i3
-      ./module/home-manager
-      ./module/x11.nix
-      (import "${home-manager}/nixos")
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./module/i3
+    ./module/home-manager
+    ./module/users.nix
+    ./module/x11.nix
+    (import "${home-manager}/nixos")
+  ];
 
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
     enable = true;
@@ -21,14 +24,14 @@ in
     efiSupport = true;
   };
 
-  networking.hostName = "twink"; # Define your hostname.
+  networking.hostName = "twink";
   networking.networkmanager.enable = true;
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
 
   programs.firefox.enable = true;
 
-  # You can use https://search.nixos.org/ to find more packages (and options).
+  # https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     vim
     curl
@@ -45,4 +48,3 @@ in
 
   system.stateVersion = "25.11"; # DO NOT CHANGE
 }
-
