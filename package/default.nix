2fee155391d2e7e98e694344329ed5f8e888aa29
Eva Harris <mail@gastrodon.io>
2026-01-05 15:05:55 -0500

    tidy: fmt package default

--- package/default.nix
+++ package/default.nix
@@ -1,10 +1,12 @@
 { pkgs, lib, ... }:
 let
-  cmdPackages = import ./cmd { inherit pkgs lib; };
+  cmd = import ./cmd { inherit pkgs lib; };
   sysinfo = import ./sysinfo { inherit pkgs lib; };
 in
 {
-  cmd = builtins.attrValues cmdPackages;
-  pkgs = builtins.attrValues cmdPackages ++ [ sysinfo ];
-  inherit sysinfo;
+  pkgs = builtins.attrValues cmd ++ [ sysinfo ];
+  inherit
+    cmd
+    sysinfo
+    ;
 }
