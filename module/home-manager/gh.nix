55c965c47fbcae3c920152c43468a9c189469e7e
Eva Harris <mail@gastrodon.io>
2026-01-21 21:31:48 -0500

    update gh settings

--- module/home-manager/gh.nix
+++ module/home-manager/gh.nix
@@ -5,11 +5,18 @@
 
     settings = {
       version = "1";
-      git_protocol = "https";
-      editor = "vim";
+      git_protocol = "ssh";
+      editor = "";
       prompt = "enabled";
+      prefer_editor_prompt = "disabled";
       pager = "less";
       aliases = { };
+      http_unix_socket = "";
+      browser = "firefox";
+      color_labels = "enabled";
+      accessible_colors = "disabled";
+      accessible_prompter = "disabled";
+      spinner = "enabled";
     };
   };
 }
