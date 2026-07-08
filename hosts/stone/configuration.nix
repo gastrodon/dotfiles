400954c42dcdb7532b86bad4b1baa5c0598564bb
Eva Harris <mail@gastrodon.io>
2026-07-07 20:05:23 -0400

    layout the monitors that are on

--- hosts/stone/configuration.nix
+++ hosts/stone/configuration.nix
@@ -27,7 +27,49 @@
     ];
     startup = [
       {
-        command = "${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 2560x1440 --rotate right --pos 0x1080 --output DP-0 --primary --mode 1920x1080 --rotate normal --pos 880x0 --output DP-3 --mode 2560x1440 --rotate normal --pos 1440x1763";
+        command = toString (
+          let
+            monitors = {
+              "DP-4" = "--mode 2560x1440 --rotate right --pos 0x1080";
+              "DP-3" = "--mode 2560x1440 --rotate normal --pos 1440x1763";
+            };
+            # Unspecified connected outputs default to 1080p and stack directly
+            # above DP-4 (bottom edge at y = 1080), left-to-right from x = 0.
+            stack = {
+              bottomY = 1080;
+              startX = 0;
+              width = 1920;
+              height = 1080;
+            };
+            xrandr = "${pkgs.xorg.xrandr}/bin/xrandr";
+          in
+          pkgs.writeShellScript "stone-monitor-layout" ''
+            connected=$(${xrandr} --query | ${pkgs.gawk}/bin/awk '/ connected/ {print $1}')
+
+            args=()
+            ${lib.concatStringsSep "\n" (
+              lib.mapAttrsToList (name: opts: ''
+                if printf '%s\n' "$connected" | grep -qx ${lib.escapeShellArg name}; then
+                  args+=(--output ${name} ${opts})
+                fi
+              '') monitors
+            )}
+
+            x=${toString stack.startX}
+            y=$((${toString stack.bottomY} - ${toString stack.height}))
+            for out in $connected; do
+              case "$out" in
+                ${lib.concatStringsSep "|" (lib.attrNames monitors)}) continue ;;
+              esac
+              args+=(--output "$out" --mode ${toString stack.width}x${toString stack.height} --rotate normal --pos "''${x}x$y")
+              x=$((x + ${toString stack.width}))
+            done
+
+            if [ ''${#args[@]} -gt 0 ]; then
+              ${xrandr} "''${args[@]}"
+            fi
+          ''
+        );
         notification = false;
       }
     ];
